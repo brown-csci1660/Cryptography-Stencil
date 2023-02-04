e5476b45533b89bcced8d5605ff6cd6047b002bd
Nicholas DeMarinis <ndemarinis@wpi.edu>
2023-02-03 19:50:20 -0500

    Updated stencils for grades and ivy.

--- grades/stencil/go/sol.go
+++ grades/stencil/go/sol.go
@@ -44,4 +44,22 @@ func main() {
 		// TODO: Remove this line (or it will make everything very slow)
 		fmt.Printf("Read %d bytes:  %x\n", b, bytes)
 	}
+
+	// TODO: Perhaps do some computation, then
+	// print out answers for the required questions, using the format
+	// specified in the assignment (use the helper function below!)
+	//PrintAnswer(...)
+}
+
+func PrintAnswer(totalBlocks int, a []byte, b []byte, c []byte, n []byte,
+	famousACount int, famousBCount int, famousCCount int, famousNCount int) {
+	fmt.Println("Total blocks:", totalBlocks)
+	fmt.Printf("Ciphertext for grade A: %x\n", a)
+	fmt.Printf("Ciphertext for grade B: %x\n", b)
+	fmt.Printf("Ciphertext for grade C: %x\n", c)
+	fmt.Printf("Ciphertext for grade N: %x\n", n)
+	fmt.Println("Famous student As", famousACount)
+	fmt.Println("Famous student Bs", famousBCount)
+	fmt.Println("Famous student Cs", famousCCount)
+	fmt.Println("Famous student Ns", famousNCount)
 }
--- ivy/stencil/go/sol.go
+++ ivy/stencil/go/sol.go
@@ -1,62 +1,88 @@
 package main
 
 import (
+	"bufio"
+	"encoding/hex"
 	"fmt"
 	"os"
 	"os/exec"
 )
 
 // Helper function to handle checking errors returned by functions. If err is
-// not nil, prints out the given msg to stderr along with a string version of
-// err and terminates the program with exit code 1.
+// not nil, Go aborts by calling panic()
 func checkError(err error, msg string) {
 	if err != nil {
-		fmt.Fprintf(os.stderr, "%s: %v\n", msg, err)
-		os.Exit(1)
+		panic(err)
 	}
 }
 
+// Helper to build arrays of bytes from hex strings
+// (like those that you type when interacting with the
+// router binary)
+func hexStringToBytes(s string) []byte {
+	bytes, err := hex.DecodeString(s)
+	checkError(err, "Error decoding hex string to bytes")
+
+	return bytes
+}
+
 func main() {
-	// Check that we have a valid number of arguments:
 	if len(os.Args) != 2 {
-		fmt.Printf("%v <path to ivy binary to attack>\n", os.Args[0])
+		fmt.Printf("%v <path to router binary to attack>\n", os.Args[0])
 		os.Exit(1)
 	}
 
-	// Prepare to execute the Ivy binary:
-	ivyPath    := os.Args[1]
-	ivyProcess := exec.Command(ivyPath)
+	// Idea: we can perform our attack automatically by running
+	// the router binary as a subprocess--this allows us to
+	// repeatedly send commands and read the output as if we were
+	// entering input manually, just much faster.
 
-	// Open a stdin pipe to the Ivy binary:
-	IVY_STDIN, err := ivyProcess.StdinPipe()
-	checkError(err, "Could not open stdin for Ivy binary")
-	defer IVY_STDIN.Close()
+	binPath := os.Args[1]
+	ivyProcess := exec.Command(binPath)
 
-	// Open a stdout pipe to the Ivy binary:
-	IVY_STDOUT, err := ivyProcess.StdoutPipe()
-	checkError(err, "Could not open stdout for Ivy binary")
-	defer IVY_STDOUT.Close()
+	// Set up a pipe to the router's stdin
+	ivyStdin, err := ivyProcess.StdinPipe()
+	checkError(err, "Could not open stdin")
+	defer ivyStdin.Close()
 
-	// Open a stderr pipe to the Ivy binary:
-	IVY_STDERR, err := ivyProcess.StderrPipe()
-	checkError(err, "Could not open stderr for Ivy binary")
-	defer IVY_STDERR.Close()
+	// Set up a pipe to the router process' stdout
+	ivyStdout, err := ivyProcess.StdoutPipe()
+	checkError(err, "Could not open stdout")
+	defer ivyStdout.Close()
 
-	// Run the Ivy binary:
+	// Set up a pipe to the router process' stderr
+	ivyStderr, err := ivyProcess.StderrPipe()
+	checkError(err, "Could not open stderr")
+	defer ivyStderr.Close()
+
+	// Create a buffered reader to read from stdout more easily
+	reader := bufio.NewReader(ivyStdout)
+
+	// Run the Ivy binary
 	err = ivyProcess.Start()
 	checkError(err, "Could not run Ivy binary")
-	defer ivyProcess.Wait()
 
-	//
 	// TODO: Implement your attack here.
 	//
-	// You can send data to the STDIN of the ivy binary with the following:
+	// You can send data to the stdin of the ivy binary with the following:
 	//
-	//   IVY_STDIN.puts("data to send")
+	//   ivyStdin.Write([]byte("data to send\n"))
 	//
-	// You can read the next line from the ivy binary's STDOUT or STDERR with the
-	// following:
+	// You can read the next line from the ivy binary's STDOUT with
+	// the following:
 	//
-	//   IVY_STDOUT.gets
+	//   stdoutReader.ReadString('\n')  // Note: the single quotes are important!
+	//                                  // ' denotes single character.
+	//                                  // " denotes a string
 	//
+
+	// Here's an example of how to use ReadString to get the first line
+	// the router prints to stdout
+	firstResponse, err := reader.ReadString('\n') // Read until newline character
+	checkError(err, "Error reading first response")
+
+	firstIv := hexStringToBytes(firstResponse[0:4])
+	firstCiphertext := hexStringToBytes(firstResponse[5:21])
+	fmt.Printf("First IV:  %x, First c:  %x\n", firstIv, firstCiphertext)
+
 }
